cd43b9f5ec9d5fa4494baba59bfc8d0a8ba304ee
Tony Chern <run1@aslugs.com>
2018-06-27 17:51:16 +0000

    062118

---
+++ m04/doWhile.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=1, b=2;
+    do {
+        cout << a << ' ' << b << endl;
+        a++;
+    } 
+    while ( a<3 ); 
+    
+    cout << "\nDone!";
+}
