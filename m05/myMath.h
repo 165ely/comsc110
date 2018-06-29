1e4f43ea3d950253bd39bce6f0d47123d2b4cea2
Tony Chern <run1@aslugs.com>
2018-06-29 04:17:43 +0000

    062818

---
+++ m05/myMath.h
@@ -0,0 +1,8 @@
+#ifndef MYMATH_H
+#define MYMATH_H
+
+double mult(double x, double y) {
+    return (x * y);
+}
+
+#endif
---
+++ m05/usingIncludes.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "myMath.h"
+using namespace std;
+
+int main() {
+    double a=1, b=2;
+    cout << mult(a,b);
+}
